f9ff2e953b5969d0727f892fdbc24d55c87eebdb
VaheP <vahe.petrosyan03@gmail.com>
2023-12-02 19:28:35 +0400

    Performance improvements

--- src/genetic_algorithm_for_image_reconstruction.py
+++ src/genetic_algorithm_for_image_reconstruction.py
@@ -22,7 +22,7 @@ def genetic_algorithm_image_reconstruction(target_image, initial_population, gen
     :return: The best image from the last generation.
     """
     current_population = initial_population
-    best_image = None
+    best_image = current_population[0]
     best_fitness = float('inf')
 
     for generation in range(generations):
@@ -30,28 +30,31 @@ def genetic_algorithm_image_reconstruction(target_image, initial_population, gen
         fitness_scores = []
         for individual in current_population:
             fitness = combine_metrics({'mse': True}, individual, target_image)
-            fitness_scores.append((individual, fitness))
-            if fitness < best_fitness:
+            fitness_scores.append(fitness)
+            if fitness > best_fitness:
                 best_fitness = fitness
                 best_image = individual
 
         # Selection
-        selected_pairs = selector(fitness_scores, lambda x: x[1], method='best')
+        selected_pairs = selector(current_population, fitness_scores, method='best')
 
-        print(selected_pairs[0][0])
         # Crossover and Mutation
         next_generation = []
+        if len(selected_pairs) % 2 == 1:
+            selected_pairs.append(selected_pairs[-1])
+        print(len(selected_pairs))
+
         for pair in selected_pairs:
-            img1, img2 = pair[0], pair[0]
+            img1, img2 = pair[0][0], pair[1][0]
             # Crossover
             child = blend_crossover(img1, img2)
             # Mutation
             if random.random() < mutation_prob:
                 child = image_mutation(child, pixel_mutation_prob=0.1, shape_mutation_prob=0.1,
                                        pixel_altering_prob=0.1, max_pixel_range=255, number_of_shapes=1)
-            next_generation.append(child)
-
-        current_population = next_generation
+            # next_generation.append(child)
+            current_population.append(child)
+        # current_population = next_generation
 
     return best_image
 
@@ -70,6 +73,6 @@ if __name__ == '__main__':
     target_image = Image.open('images/mona_lisa.jpg')
     dummy_population = dummy_initial_population(target_image, target_image.size, 100)
 
-    # save the first image in the population
-    dummy_population[0].save('images/mona_lisa_0.jpg')
-    genetic_algorithm_image_reconstruction(target_image, dummy_population, generations=10, mutation_prob=0.2)
+    result = genetic_algorithm_image_reconstruction(target_image, dummy_population, generations=10, mutation_prob=0.2)
+    print(result)
+    result.save('images/mona_lisa_1.jpg')
--- src/selector.py
+++ src/selector.py
@@ -1,8 +1,22 @@
-def selector(populations, fit_func, method='best'):
-    fitness_scores = [(population, fit_func(population)) for population in populations]
+# def selector(populations, fit_func, method='best'):
+#     fitness_scores = [(population, fit_func(population)) for population in populations]
+#
+#     sorted_by_fitness = sorted(fitness_scores, key=lambda x: x[1], reverse=(method == 'best'))
+#
+#     paired_populations = []
+#     for i in range(0, len(sorted_by_fitness), 2):
+#         paired_populations.append(tuple(sorted_by_fitness[i:i + 2]))
+#
+#     return paired_populations
 
-    sorted_by_fitness = sorted(fitness_scores, key=lambda x: x[1], reverse=(method == 'best'))
+def selector(populations, fitness_values, method='best'):
+    # Pair each population with its fitness score
+    paired_fitness = list(zip(populations, fitness_values))
 
+    # Sort the pairs by fitness score
+    sorted_by_fitness = sorted(paired_fitness, key=lambda x: x[1], reverse=(method == 'best'))
+
+    # Pair the populations for selection
     paired_populations = []
     for i in range(0, len(sorted_by_fitness), 2):
         paired_populations.append(tuple(sorted_by_fitness[i:i + 2]))
