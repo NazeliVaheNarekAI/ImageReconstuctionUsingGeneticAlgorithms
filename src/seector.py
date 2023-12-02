d0cf9c4c56b0c33e4dd45156b0351ef06be01149
Narek <meloyann87@gmail.com>
2023-12-02 11:30:05 +0400

    Narek's Codes

---
+++ src/seector.py
@@ -0,0 +1,10 @@
+def selector(populations, fit_func, method='best'):
+    fitness_scores = [(population, fit_func(population)) for population in populations]
+
+    sorted_by_fitness = sorted(fitness_scores, key=lambda x: x[1], reverse=(method == 'best'))
+
+    paired_populations = []
+    for i in range(0, len(sorted_by_fitness), 2):
+        paired_populations.append(tuple(sorted_by_fitness[i:i+2]))
+
+    return paired_populations
