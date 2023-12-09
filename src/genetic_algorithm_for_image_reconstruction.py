2979d3a04c331f6a2e1c913aee6ab3e2171ccf4d
VaheP <vahe.petrosyan03@gmail.com>
2023-12-09 20:33:08 +0400

    Code refactoring"

--- src/genetic_algorithm_for_image_reconstruction.py
+++ src/genetic_algorithm_for_image_reconstruction.py
@@ -12,30 +12,40 @@ from tqdm import tqdm
 import methods as methods
 
 
-def gen_alg(target_image):
-    """
-    :param target_image: The image that the genetic algorithm will attempt to reconstruct.
-    :return: The genetically reconstructed image that closely resembles the target image.
-    """
-    return genetic_algorithm_image_reconstruction(target_image, generations=250,
-                                                  mutation_prob=0.1, initial_population_size=50)
-
-
 # Defining the Genetic Algorithm for Image Reconstruction
-def genetic_algorithm_image_reconstruction(target_image, generations, mutation_prob, initial_population_size):
+def genetic_algorithm_image_reconstruction(target, generations, mutation_prob, initial_population_size,
+                                           fitness_options=None,
+                                           mutation_options=None):
     """
-    A genetic algorithm for image reconstruction.
+    Reconstructs an image using genetic algorithm.
 
-    :param target_image: The target image to reconstruct.
-    :param initial_population: A list of initial population images.
-    :param generations: Number of generations to run the GA.
-    :param mutation_prob: Probability of mutation.
-    :return: The best image from the last generation.
+    :param target: The target image to reconstruct.
+    :type target: PIL.Image.Image
+    :param generations: The number of generations to evolve.
+    :type generations: int
+    :param mutation_prob: The probability of mutation.
+    :type mutation_prob: float
+    :param initial_population_size: The initial size of the population.
+    :type initial_population_size: int
+    :param fitness_options: Options for fitness evaluation.
+    :type fitness_options: dict, optional
+    :param mutation_options: Options for mutation.
+    :type mutation_options: dict, optional
+    :return: The reconstructed image.
+    :rtype: PIL.Image.Image
     """
 
-    unique_colors, frequency = compute_color_distribution(target_image)
+    if mutation_options is None:
+        mutation_options = {'pixel_mutation_prob': 0.3, 'shape_mutation_prob': 0.7,
+                            'pixel_altering_prob': 0.5, 'max_pixel_range': 125,
+                            'number_of_shapes': 3}
+    if fitness_options is None:
+        fitness_options = {'psnr': False, 'ssim': False, 'delta_e': False,
+                           'mse': True}
 
-    initial_population = create_population(unique_colors, frequency, target_image.size, initial_population_size)
+    unique_colors, frequency = compute_color_distribution(target)
+
+    initial_population = create_population(unique_colors, frequency, target.size, initial_population_size)
     current_population = initial_population
     best_image = current_population[0]
     best_fitness = float('-inf')
@@ -46,7 +56,7 @@ def genetic_algorithm_image_reconstruction(target_image, generations, mutation_p
         fitness_scores = []
 
         for individual in current_population:
-            fitness = calculate_fitness(individual, target_image)
+            fitness = calculate_fitness(individual, target, fitness_options)
             fitness_scores.append(fitness)
             if fitness > best_fitness:
                 print("New Best Fitness", fitness)
@@ -58,15 +68,15 @@ def genetic_algorithm_image_reconstruction(target_image, generations, mutation_p
         selected_pairs = selector.get_parents(current_population, fitness_scores)
 
         if gen % 50 == 0:
-            best_image.save('images/mona_lisa_result_' + str(gen) + '.jpg')
+            best_image.save('images/result_' + str(gen) + '.jpg')
 
         results = []
         for pair in selected_pairs:
-            results.append(process_pair(pair, mutation_prob, unique_colors, frequency))
+            results.append(process_pair(pair, mutation_prob, unique_colors, frequency, **mutation_options))
         for child in results:
             # child.show()
             current_population.append(child)
-            fitness_scores.append(calculate_fitness(child, target_image))
+            fitness_scores.append(calculate_fitness(child, target, fitness_options))
 
         # Elitism
         sorted_by_fitness = sorted(zip(current_population, fitness_scores), key=lambda x: x[1], reverse=True)
@@ -75,25 +85,34 @@ def genetic_algorithm_image_reconstruction(target_image, generations, mutation_p
     return best_image
 
 
-def calculate_fitness(image, target_image):
+def calculate_fitness(image, target, options=None):
     """
-    Calculate the fitness of an image compared to the target image.
+    Calculate the fitness of an image compared to a target image.
 
-    :param image: The image to calculate the fitness of.
-    :param target_image: The target image.
-    :return: The fitness score.
+    :param image: The image that will be evaluated.
+    :param target: The target image to compare against.
+    :param options: A dictionary containing the options for calculating fitness. Default is {'psnr': False, 'ssim': False, 'delta_e': False, 'mse': True}.
+    :return: The calculated fitness value based on the given options.
     """
-    return combine_metrics({'psnr': False, 'ssim': False, 'delta_e': False, 'mse': True}, image, target_image)
+    if options is None:
+        options = {'psnr': False, 'ssim': False, 'delta_e': False, 'mse': True}
+    return combine_metrics(options, image, target)
 
 
-def process_pair(pair, mutation_prob, unique_colors, frequency):
+def process_pair(pair, mutation_prob, unique_colors, frequency, pixel_mutation_prob=0.3, shape_mutation_prob=0.7,
+                 pixel_altering_prob=0.5, max_pixel_range=125, number_of_shapes=3):
     """
     Process a pair of images.
 
-    :param pair: The pair of images.
-    :param mutation_prob: The probability of mutation.
-    :param unique_colors: The number of unique colors in the images.
-    :param frequency: The frequency of the images.
+    :param pair: A tuple containing two images to be processed.
+    :param mutation_prob: The probability of applying mutation to the child image.
+    :param unique_colors: A list of unique colors used in the images.
+    :param frequency: A dictionary containing the frequency of each color in the images.
+    :param pixel_mutation_prob: The probability of applying pixel-level mutation.
+    :param shape_mutation_prob: The probability of applying shape-level mutation.
+    :param pixel_altering_prob: The probability of altering each individual pixel value.
+    :param max_pixel_range: The maximum range of pixel values for altering pixel values.
+    :param number_of_shapes: The number of shapes to be added in shape-level mutation.
     :return: The processed child image.
     """
     img1, img2 = pair[0], pair[1]
@@ -102,9 +121,9 @@ def process_pair(pair, mutation_prob, unique_colors, frequency):
                                    'random_row_column': 0})
     # Mutation
     if random.random() < mutation_prob:
-        child = image_mutation(child, pixel_mutation_prob=0.3, shape_mutation_prob=0.7,
-                               pixel_altering_prob=0.5, max_pixel_range=125, number_of_shapes=3,
-                               unique_colors=unique_colors, frequency=frequency)
+        child = image_mutation(child, pixel_mutation_prob, shape_mutation_prob,
+                               pixel_altering_prob, max_pixel_range, number_of_shapes,
+                               unique_colors, frequency)
     return child
 
 
@@ -126,15 +145,36 @@ def create_population(unique_colors, frequency, size, num_individuals):
     # return [generate_noise_from_distribution(unique_colors, frequency, size) for _ in range(num_individuals)]
 
 
-if __name__ == '__main__':
-    use_chunking_mode = True   # set this to True to use the Chunking modem, False to use the normal mode
+def gen_alg_for_chunking(target, generations=250, mutation_prob=0.1, initial_population_size=50):
+    """
+    :param target: The image that needs to be reconstructed.
+    :param generations: The number of generations for the genetic algorithm. Default value is 250.
+    :param mutation_prob: The probability of mutation for each individual in the population. Default value is 0.1.
+    :param initial_population_size: The size of the initial population for the genetic algorithm. Default value is 50.
+    :return: The reconstructed image obtained using the genetic algorithm.
 
+    """
+    fit = {'psnr': False, 'ssim': False, 'delta_e': False, 'mse': True}
+    mut = {'pixel_mutation_prob': 0.3, 'shape_mutation_prob': 0.7, 'pixel_altering_prob': 0.5,
+           'max_pixel_range': 125, 'number_of_shapes': 3}
+
+    return genetic_algorithm_image_reconstruction(target, generations, mutation_prob, initial_population_size,
+                                                  fit, mut)
+
+
+if __name__ == '__main__':
+    use_chunking_mode = False  # set this to True to use the Chunking mode, False to use the normal mode
+
+    fitness = {'psnr': False, 'ssim': False, 'delta_e': False, 'mse': True}
+    mutation = {'pixel_mutation_prob': 0.3, 'shape_mutation_prob': 0.7, 'pixel_altering_prob': 0.5,
+                'max_pixel_range': 125, 'number_of_shapes': 3}
     if not use_chunking_mode:
         target_image = Image.open('images/LittlePrince.jpg')
         initial_size = target_image.size
         target_image = target_image.resize((200, target_image.size[1] * 200 // target_image.size[0]))
         result = genetic_algorithm_image_reconstruction(target_image, generations=16000,
-                                                        mutation_prob=0.2, initial_population_size=50)
+                                                        mutation_prob=0.2, initial_population_size=50,
+                                                        fitness_options=fitness, mutation_options=mutation)
         result = result.resize(initial_size)
         result.save('images/result.jpeg')
 
@@ -143,6 +183,6 @@ if __name__ == '__main__':
         base_size = 600
         resized_target = target_image.resize((base_size, target_image.size[1] * base_size // target_image.size[0]))
         print(resized_target.size)
-        result = methods.chunking_with_padding(resized_target, 25, 25, gen_alg, blend_width=10)
+        result = methods.chunking_with_padding(resized_target, 25, 25, gen_alg_for_chunking, blend_width=10)
         result = result.resize(target_image.size)
         result.save('images/result.jpeg')
